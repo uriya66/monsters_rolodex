469cdd4ad12da73fa796445e7a06862c04dad086
uriya66 <uriya66@gmail.com>
2023-07-16 20:35:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
+
+const App = () => {
+  // useState return array of [value, setValue]
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // 2 arguments 1 callback function and second its array
+  useEffect(() => {
+    let usersUrl = "https://jsonplaceholder.typicode.com/users";
+    // fetch() - starts the process of fetching a resource from the network, returning a promise which is fulfilled once the response is available.
+    fetch(usersUrl)
+      // if is true return the information from JSON file
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    // gives the new array of users by search
+    // filter return array that contains a list of elements that run the filter function on every element in the array
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Monsters Rolodex</h1>
+
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeHolder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import "./search-box.styles.css";
+
+const SearchBox = ({ className, placeHolder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeHolder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default SearchBox;
